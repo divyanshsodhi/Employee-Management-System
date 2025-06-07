6301c0d5ff8754cdb736bbc8e66a00d85717db64
Divyansh <sodhidivyansh80@gmail.com>
2025-06-07 21:48:35 +0530

    Admin-Dashboard Completed

--- src/Components/Others/CreateTask.jsx
+++ src/Components/Others/CreateTask.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const CreateTask = () => {
   return (
-    <div >
-                <form action="" className='flex flex-wrap w-full items-start justify-between'> 
-                   <div className='bg-zinc-700 w-full mt-10 flex sm:gap-10 md:justify-between lg:justify-between gap-10 p-10 items-center rounded-xl  lg:px-60 '>
+    <div className=''>
+                <form action="" className='flex flex-wrap w-full items-start justify-between '> 
+                   <div className='bg-zinc-700 w-full mt-10 flex sm:gap-5 md:justify-between lg:justify-between gap-10 p-10 items-center rounded-xl  lg:px-60 '>
                         <div className="left">
                           <h3>Task Title</h3>  
                           <input className='border-1 border-zinc-300 rounded-xl p-2' type="text" placeholder="Make a UI Design" name="" id="" />
@@ -17,8 +17,8 @@ const CreateTask = () => {
                         </div>
                         <div className="right flex flex-col">
                             <h3 className=''>Description</h3>
-                           <textarea  cols={40} rows={10} placeholder="Give the description of the Job assigned" className='bg-transparent placeholder:text-slate-300 resize-none ' name="" id=""></textarea>
-                           <button className='bg-emerald-700 px-3 py-2 mt-2 rounded-2xl w-full'>Create Task</button>
+                           <textarea  cols={40} rows={10} placeholder="Give the description of the Job assigned" className=' bg-transparent placeholder:text-slate-300 resize-none border-2 rounded-xl border-white' name="" id=""></textarea>
+                           <button className='bg-emerald-700 px-3 py-2 mt-2 rounded-2xl w-full b'>Create Task</button>
                         </div>
                    </div>
 
